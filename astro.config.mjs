5be2dba63aac98356cd50ea61ea93e6bec833542
adolfoalvar3z <adolfo.alvar3z.p@gmail.com>
2026-03-11 19:37:10 -0300

    feat: Add React integration and configure Astro output mode to hybrid for development.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 
 const isBuild = process.argv.includes('build');
 
 export default defineConfig({
     site: 'https://h4ck.cl',
+    output: isBuild ? 'static' : 'hybrid',
     integrations: [
+        react(),
         isBuild ? null : keystatic()
     ],
     vite: {
         plugins: [tailwindcss()]
     }
 });
+
