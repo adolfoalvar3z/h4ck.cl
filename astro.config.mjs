0617df2036bf4b6818a149135a437870cd2fe88b
adolfoalvar3z <adolfo.alvar3z.p@gmail.com>
2026-03-11 21:38:53 -0300

    upa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-// @ts-check
+﻿// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
@@ -9,7 +9,7 @@ const isBuild = process.argv.includes('build');
 export default defineConfig({
     site: isBuild ? 'https://adolfoalvar3z.github.io' : 'http://localhost:4321',
     base: isBuild ? '/h4ck.cl/' : '/',
-    output: isBuild ? 'static' : 'hybrid',
+    output: isBuild ? 'hybrid' : 'hybrid',
     integrations: [
         react(),
         isBuild ? null : keystatic()
